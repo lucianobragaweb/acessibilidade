e27f5ce3fc650f9b5448e2c1f7135dde3d2bfbd5
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-08-04 16:13:33 -0300

    Adicionado LICENSE

--- fonte.js
+++ fonte.js
@@ -1,6 +1,6 @@
 function fonte(e){
-	var elemento = $(".acessibilidade");
-	var fonte = elemento.css('font-size');
+	var elemento 	= $(".acessibilidade");
+	var fonte 		= elemento.css('font-size');
 	if (e == 'a') {
 		elemento.css("fontSize", parseInt(fonte) + 1);
 	} else if('d'){
